9d7d6bc594e47525b57b1a9557baba7ef1e1415c
yinte12 <yinte112@163.com>
2016-10-11 18:00:25 +0800

    add view

--- app/src/main/java/com/asuper/onlyplayer/MainActivity.java
+++ app/src/main/java/com/asuper/onlyplayer/MainActivity.java
@@ -1,13 +1,78 @@
 package com.asuper.onlyplayer;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.asuper.onlyplayer.media.IjkVideoView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private IjkVideoView mView;
+    private Button mBtn;
+    private TextView mText;
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mView = (IjkVideoView) findViewById(R.id.view);
+
+        mBtn = (Button) findViewById(R.id.button);
+        mText = (TextView) findViewById(R.id.text);
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("video/*");//设置类型，我这里是任意类型，任意后缀的可以这样写。
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent,1);
+            }
+        });
+        videoView = (VideoView) findViewById(R.id.video);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();//得到uri，后面就是将uri转化成file的过程。
+            String[] proj = {MediaStore.Images.Media.DATA};
+            Cursor actualimagecursor = managedQuery(uri, proj, null, null, null);
+            int actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            actualimagecursor.moveToFirst();
+            String img_path = actualimagecursor.getString(actual_image_column_index);
+            String VideoPath= Environment.getExternalStorageDirectory()
+                    .getAbsolutePath() + "/使徒行者HD粤语中字.mp4";
+
+
+            File file = new File(img_path);
+            Log.i(TAG, "file is " + file.exists());
+            mText.setText(img_path);
+
+            videoView.setVideoPath(img_path);
+            videoView.start();
+            mView.setVideoPath(img_path);
+            mView.start();
+        }
+    }
+
+    private String getLocalVideoPath(String name) {
+        String sdCard = Environment.getExternalStorageDirectory().getPath();
+        String uri = sdCard + File.separator + name;
+        return uri;
     }
 }
